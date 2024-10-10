b8a2b2a02af9d14cf49b580740bf4bb34332b9c7
William Galvis <williamsoluntech@gmail.com>
2024-10-10 18:49:04 -0500

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: '03_flash_mobile',
+  appId: 'flashing.es.app',
+  appName: 'Flashing',
   webDir: 'dist'
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,41 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
-
+import { IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonText, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import { CapacitorFlash } from '@capgo/capacitor-flash';
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+	const [isLight, setIsLight] = React.useState(false);
+
+	const switchLight = async (value: boolean) => {
+		setIsLight(value);
+		if(value) {
+			await CapacitorFlash.switchOn({intensity: 100});
+		} else {
+			await CapacitorFlash.switchOff();
+		}
+	};
+
+	return (
+		<IonPage>
+			<IonContent fullscreen>
+				<IonHeader>
+					<IonToolbar className='ion-text-center'>
+						<IonTitle>Flash</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<IonGrid>
+					<IonRow>
+						<IonCol className='ion-text-center'>
+							<IonText>{isLight ? 'La linterna esta encendida' : 'La linterna esta apagada'}</IonText>
+						</IonCol>
+					</IonRow>
+					<IonRow>
+						<IonCol className='ion-text-center ion-margin-top'>
+							<IonToggle checked={isLight} onIonChange={(e) => switchLight(e.detail.checked)} />
+						</IonCol>
+					</IonRow>
+				</IonGrid>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Home;
