ecaf14fdc0a9a848e630d719782b96857baf5d8e
William Galvis <williamsoluntech@gmail.com>
2024-10-10 18:20:34 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '03_flash_mobile',
+  webDir: 'dist'
+};
+
+export default config;
